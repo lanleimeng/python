db74faa5aebcafad876b136dd5e8546a1b17cc2d
lanleimeng <133726463+lanleimeng@users.noreply.github.com>
2026-03-09 14:55:07 +0700

    test

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "hello world"
 
+@app.route('/analisa-ramon')
+def analisa_data():
+    return 'test'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
